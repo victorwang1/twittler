8f48587468e230db1cc8774d113c8862581a4838
Victor Wang <victor.wang@me.com>
2017-07-31 01:06:04 +1000

    implement auto-update feed and time

--- application.js
+++ application.js
@@ -2,40 +2,64 @@ $(document).ready(function(){
   var $body = $('body');
   $body.html('');
 
-// generate tweet
-  var newTweet = function(tweet) {
-    var $tweet = $('<section class="tweet"></section>');
-    var $time = $('<div class="time"></div>');
-    var $userName = $('<span class="username"></span>');
-    var now = moment();
 
+// generate tweet
+  var formatTweet = function(tweet) {
+    var $tweet = $('<section class="tweet"></section>');
+    var $time = $('<div class="time" data-created="' + tweet.created_at + '"></div>');
+    var $userName = $('<span class="username"></span>');
+
+    $tweet.text(': ' + tweet.message);
     $userName.text('@' + tweet.user);
-    $tweet.text($userName.text() + ': ' + tweet.message);
-    $time.text(moment(now).fromNow());
+    $userName.prependTo($tweet);
+
+    // timestamp
+    $time.text(moment(tweet.created_at).fromNow());
     $time.prependTo($tweet);
 
     return $tweet;
   }
 
+
 // initialize
+  var feedLength = streams.home.length;
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
-    var $tweet = newTweet(tweet);
+    var $tweet = formatTweet(tweet);
 
     $tweet.appendTo($body);
 
     index -= 1;
   }
 
-// update tweet
-  var update = function() {
 
+// update tweet
+  var updateFeed = function() {
+    var newFeedLength = streams.home.length;
+    if (newFeedLength > feedLength) {
+      for (feedLength; feedLength < newFeedLength; feedLength++) {
+        formatTweet(streams.home[feedLength]).prependTo($body);
+      }
+      feedLength++;
+    }
   }
-  var refreshFeed = setInterval(update, 1000);
+
+  var updateTime = function() {
+    $('.time').each(function(element) {
+      var created_at = $(this).data('created');
+      console.log(created_at);
+      $(this).text(moment(created_at).fromNow());
+    });
+  }
+
+  var refreshFeed = setInterval(updateFeed, 500);
+  var refreshTime = setInterval(updateTime, 10000);
 
 
 // events
-
+  $('.username').hover(function() {
+    $(this).toggleClass('highlight');
+  });
 
 });
