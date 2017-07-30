2b0347e717099ed6c847b5a9732a7b4a0cebdb9e
Victor Wang <victor.wang@me.com>
2017-07-30 18:06:50 +1000

    move script into seperate file

---
+++ application.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  var $body = $('body');
+  $body.html('');
+
+  var index = streams.home.length - 1;
+  while(index >= 0){
+    var tweet = streams.home[index];
+    var $tweet = $('<div></div>');
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $tweet.appendTo($body);
+    index -= 1;
+  }
+
+  
+});
