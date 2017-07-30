dd60806a126375eda9339b84537583f041244a04
Victor Wang <victor.wang@me.com>
2017-07-30 23:23:38 +1000

    refactor page initialization

--- application.js
+++ application.js
@@ -2,14 +2,40 @@ $(document).ready(function(){
   var $body = $('body');
   $body.html('');
 
+// generate tweet
+  var newTweet = function(tweet) {
+    var $tweet = $('<section class="tweet"></section>');
+    var $time = $('<div class="time"></div>');
+    var $userName = $('<span class="username"></span>');
+    var now = moment();
+
+    $userName.text('@' + tweet.user);
+    $tweet.text($userName.text() + ': ' + tweet.message);
+    $time.text(moment(now).fromNow());
+    $time.prependTo($tweet);
+
+    return $tweet;
+  }
+
+// initialize
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    var $tweet = newTweet(tweet);
+
     $tweet.appendTo($body);
+
     index -= 1;
   }
 
-  
+// update tweet
+  var update = function() {
+
+  }
+  var refreshFeed = setInterval(update, 1000);
+
+
+// events
+
+
 });
